68d53a877a38e9640619f54055f4e531616de676
ladamato <43681177+ladamato@users.noreply.github.com>
2021-04-23 10:29:40 -0500

    Added intialization script for use with testing in docker

---
+++ src/test/resources/docker/initialize/initialize_data.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Copyright 2021 XEBIALABS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+set -x
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+##### Insert template
+
+#wget --http-user=admin --http-password=admin --auth-no-challenge \
+#     --header="Accept: application/json" \
+#     --header="Content-type: application/json" \
+#     --post-file=$SCRIPTPATH/data/release-template-noRemoteWithVariable.json \
+#http://localhost:15516/api/v1/templates/import -O /dev/null
+
+curl --user admin:admin -i -X POST http://localhost:15516/api/v1/templates/import \
+  -H "Content-Type: application/json" -H "Accept: application/json" \
+  --data "@"$SCRIPTPATH"/data/release-template-variableSetter.json"
+
+
+
+
+
